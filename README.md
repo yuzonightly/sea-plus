3e0b0f36c4c5abda8de31bf7d1d4b454708129c7
yuzonightly <yuzonakai@gmail.com>
2022-01-17 21:08:57 +0000

    update

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# qt-tea
-A QT Application
+# sea-plus
+
+## Prerequisites
+
+Install `grpc` and `protobuf`.
+
+## Build
+
+```bash
+protoc -I . --grpc_out=. --plugin=protoc-gen-grpc=grpc_cpp_plugin ./sea.proto
+protoc -I . --cpp_out=. ./sea.proto
+```
---
+++ sea/sea_server.cc
@@ -0,0 +1,39 @@
+#include "sea.grpc.pb.h"
+#include <grpc++/grpc++.h>
+#include <memory>
+#include <iostream>
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using sample::SampleRequest;
+using sample::SampleResponse;
+using sample::SampleService;
+
+class SampleServiceImpl final : public SampleService::Service {
+    Status SampleMethod(ServerContext* context, const SampleRequest* request, SampleResponse* response) override {
+        response->set_response_sample_field("Hello " + request->request_sample_field());
+        return Status::OK;
+    }
+};
+
+void RunServer() {
+    std::string server_address{"localhost:2510"};
+    SampleServiceImpl service;
+
+    // Build server
+    ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+    std::unique_ptr<Server> server{builder.BuildAndStart()};
+
+    // Run server
+    std::cout << "Server listening on " << server_address << std::endl;
+    server->Wait();
+}
+
+int main(int argc, char** argv) {
+    RunServer();
+    return 0;
+}
