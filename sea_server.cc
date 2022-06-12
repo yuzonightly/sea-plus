60e7e2c6a79e63540b78c4441d55ec767492770d
yuzonightly <yuzonakai@gmail.com>
2022-06-12 01:15:09 +0100

    Update

--- sea_server.cc
+++ sea_server.cc
@@ -4,16 +4,19 @@
 #include <string>
 #include <thread>
 
+#include <cpprest/filestream.h>
+#include <cpprest/http_client.h>
+#include <cpprest/json.h>
+#include <cpprest/uri.h>
 #include <grpc++/grpc++.h>
 #include <grpc/support/log.h>
-
-#include "api.h"
-#include "ocean/sea.grpc.pb.h"
 
 #include <cpprest/filestream.h>
 #include <cpprest/http_client.h>
 #include <cpprest/json.h>
 #include <cpprest/uri.h>
+
+#include "ocean/sea.grpc.pb.h"
 
 using namespace utility;           // Common utilities like string conversions
 using namespace web;               // Common features like URIs.
@@ -21,7 +24,6 @@ using namespace web::http;         // Common HTTP functionality
 using namespace web::http::client; // HTTP client features
 using namespace concurrency::streams; // Asynchronous streams
 
-// using API;
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
@@ -30,6 +32,13 @@ using sea::SeaRequest;
 using sea::SeaResponse;
 using sea::SeaService;
 
+class tmdb_api {
+private:
+  std::string TMDB_API_KEY = "4faf3d9fffffffffffffffffffffffff";
+
+public:
+  tmdb_api() { std::cout << "Hello TMDB API" << std::endl; }
+};
 
 class SeaServiceImpl final : public SeaService::Service {
 
@@ -55,32 +64,69 @@ void RunServer() {
   server->Wait();
 }
 
+// void make_request(http_client &client, method mtd, json::value const &jvalue)
+// {
+//   make_task_request(client, mtd, jvalue)
+//       .then([](http_response response) {
+//         if (response.status_code() == status_codes::OK) {
+//           return response.extract_json();
+//         }
+//         return pplx::task_from_result(json::value());
+//       })
+//       .then([](pplx::task<json::value> previousTask) {
+//         try {
+//           display_json(previousTask.get(), L"R: ");
+//         } catch (http_exception const &e) {
+//           wcout << e.what() << endl;
+//         }
+//       })
+//       .wait();
+// }
+
+// MAIN
 int main(int argc, char **argv) {
   auto fileStream = std::make_shared<ostream>();
 
-  pplx::task<void> requestTask =
-      fstream::open_ostream(U("movies.json"))
+  // parameters
+  std::string query = "kimetsu";
 
+  tmdb_api tmdb = tmdb_api();
+
+  // fetch data
+  pplx::task<void> requestTask =
+      fstream::open_ostream(U("query.json"))
           .then([=](ostream outFile) {
             *fileStream = outFile;
-            http_client client(U("https://reqres.in"));
+            http_client client(U("https://api.themoviedb.org/3/search/"));
+            // std::cout << uri_builder(U("movie"))
+            //                  .append_query(U("api_key"), U(TMDB_API_KEY))
+            //                  .append_query(U("query"), U(query))
+            //                  .to_string()
+            //           << std::endl;
             return client.request(
-                methods::GET,
-                uri_builder(U("api")).append_path(U("users")).to_string());
+                methods::GET, uri_builder(U("movie"))
+                                  .append_query(U("api_key"), U(TMDB_API_KEY))
+                                  .append_query(U("query"), U(query))
+                                  .to_string());
           })
-
           .then([=](http_response response) {
             if (response.status_code() != 200) {
               throw std::runtime_error("Returned " +
                                        std::to_string(response.status_code()));
             }
             response.body().read_to_end(fileStream->streambuf()).wait();
-            return fileStream->close();
+            fileStream->close();
+            // return fileStream->close();
+            return response.extract_json();
+          })
+          .then([](json::value jsonObject) {
+            // std::cout << "Hello jsonObject" << std::endl;
+            // std::cout << jsonObject[U("first_name")].as_string()
+            //     << " " << jsonObject[U("last_name")] << std::endl;
           });
 
   try {
     while (!requestTask.is_done()) {
-      // std::cout << ".";
     }
   } catch (const std::exception &e) {
     printf("Error exception: %s\n", e.what());
